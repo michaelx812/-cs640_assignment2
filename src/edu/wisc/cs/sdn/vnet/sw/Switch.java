7836140be4c62622045088ef4cf4c8b643957aea
Ge Xu <michaelx@GematoMacBook-Pro.local>
2019-10-07 02:19:43 -0500

    pretest sw

--- src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -5,14 +5,37 @@ import edu.wisc.cs.sdn.vnet.Device;
 import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
 
+import java.util.concurrent.*; 
+
 /**
  * @author Aaron Gember-Jacobson
  */
 public class Switch extends Device implements Runnable 
 {	
+	ConcurrentHashMap<MACAddress,swEntry> swTable;
+	protected long timeout = 15000;
+
+	class swEntry{
+		long startTime;
+		Iface inIface;
+		swEntry(long l, Iface inface){
+			this.startTime = l;
+			this.inIface = inface;
+		}
+	}
+
 	@Override
 	public void run(){
-		System.out.println("running thread!");
+		while(true){
+			for(Map.Entry<MACAddress,swEntry> entry: this.wTable.entrySet()){
+				long curTime = System.currentTimeMillis();
+				long startTime = entry.getValue().starTime;
+				if((curTime - startTime)>this.timeout){
+					System.out.println("Timeout entry MAC: "+entry.getKey());
+					this.swTable.remove(entry.getKey());
+				}
+			}
+		}
 	}
 	/**
 	 * Creates a router for a specific host.
@@ -21,6 +44,7 @@ public class Switch extends Device implements Runnable
 	public Switch(String host, DumpFile logfile)
 	{
 		super(host,logfile);
+		swTable = new ConcurrentHashMap<MACAddress,swEntry>();
 		Thread thread = new Thread(this);
 		thread.start();
 	}
@@ -35,9 +59,27 @@ public class Switch extends Device implements Runnable
 		System.out.println("*** -> Received packet: " +
                 etherPacket.toString().replace("\n", "\n\t"));
 		
+
 		/********************************************************************/
 		/* TODO: Handle packets                                             */
-		
+		MACAddress srcMac = etherPacket.getSourceMac();
+		MACAddress dstMac = etherPacket.getDestinationMAC();
+		if(swTable.contains(dstMac)){
+			swEntry entry = swTable.get(dstMac);
+			entry.startTime = System.currentTimeMillis();
+			sendPacket(etherPacket, entry.inIface);
+		}else{
+			swEntry entry = new swEntry(System.currentTimeMillis(),inIface);
+			swTable.add(srcMac,entry);
+			//broadcast
+			System.out.println("broad casting!");
+			for(Map.Entry<String,Iface> interfaceEntry: this.interfaces.entrySet()){
+				Iface curIface = interfaceEntry.getValue();
+				if(!curIface.toString().equals(inIface.toString())){
+					sendPacket(etherPacket,curIface);
+				}
+			}
+		}
 		/********************************************************************/
 	}
 }
